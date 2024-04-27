730cd007bb2efa065ca246b1d08cc363b6bd2682
Pascal-K <karsten.pas@gmail.com>
2024-04-27 22:16:26 +0200

    timer fix / backwards

--- script.js
+++ script.js
@@ -2,39 +2,52 @@ const   startButton = document.getElementById('start'),
         currentTime = document.getElementById('time');
 
 
-
+let inputTime = 11;
+let timer;
 sec = '0' + 0;
 min = '0' + 0;
-var timer;
-
+min = inputTime;
 let isRunning = false;
 
-//Timer funtion
+//Start timer
 function startTimer() {
     startButton.classList.replace('fa-play', 'fa-pause');
     if (isRunning != true) {
         isRunning = true;
         timer = setInterval(() => {
-            sec++;
-            sec = sec < 10 ? '0' + sec : sec;
-
-            if (sec == 60) {
-                min++;
-                min = min < 10 ? '0' + min : min;
+            sec--;
+            if (min == 0 && sec == 0) {
+                timerStop()
+                isRunning = false
                 sec = '0' + 0;
+                min = inputTime;
+                playSound();
+
+                //Play sound TOTO
+            }
+            else if (sec <= 0) {
+                min--;
+                sec = 59;
             }
             currentTime.innerHTML = min + ":" + sec;
-        }, 1000);
+        }, 1);
     }
     else {
         timerStop();
         isRunning = false;
     }
 }
-//Pause Timer
+//Pause timer
 function timerStop() {
-    clearInterval(timer)
+    clearInterval(timer);
     startButton.classList.replace('fa-pause', 'fa-play');
+}
+
+//Play sound
+function playSound() {
+    let audio =  new Audio("assets/sounds/complete.wav");
+    audio.volume = 0.1
+    audio.play();
 }
 
 startButton.addEventListener('click', startTimer);
